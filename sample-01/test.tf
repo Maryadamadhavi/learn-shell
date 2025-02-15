838c4693a1ab1bf66f4f93c7eb8aa08d4633c21e
Maryadamadhavi <madhavim2998@gmail.com>
2025-02-15 22:17:02 +0530

    terraform

--- sample-01/test.tf
+++ sample-01/test.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred AWS region
+}
 resource "aws_instance" "my_ec2" {
   ami           = "ami-053a45fff0a704a47"  # Replace with the correct AMI ID
   instance_type = "t2.micro"
