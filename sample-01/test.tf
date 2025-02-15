6b6e35cb86b6c77275dff72193a76142733c46d9
Maryadamadhavi <madhavim2998@gmail.com>
2025-02-15 22:14:43 +0530

    terraform

--- sample-01/test.tf
+++ sample-01/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-053a45fff0a704a47"  # Replace with the correct AMI ID
   instance_type = "t2.micro"
-  vpc_security_group_ids = [sg-06202aac79deed881]
+  vpc_security_group_ids = ["sg-06202aac79deed881"]
 
   tags = {
     Name = "My-Terraform-EC2"
